69c228c82420309e35ef28f134afb62c09410665
Hobin Yoon <hobinyoon@gmail.com>
2017-09-08 15:51:24 +0000

    a bit shorter exp

--- rocksdb/quizup/quizup/simtime.cpp
+++ rocksdb/quizup/quizup/simtime.cpp
@@ -127,16 +127,11 @@ void Init2() {
     // Starting from the beginning
     // When new SSTables are created
     {
-      //double a = 9;  // fast loading
-      //double b = a + 175.0 *  50 / 1000;  // give some time for the latency to stabilize
-      //double c = b + 175.0 * 475 / 1000;  // begin the SlaAdmin with a target latency of 19 ms
-      //double d = c + 175.0 * 475 / 1000;  // keep going.
+      double a = 20.0/60;  // fast loading
+      double b = a + 40.0/60 * 10 / 100;  // give some time for the latency to stabilize
+      double c = b + 40.0/60 * 45 / 100;  // begin the SlaAdmin with a target latency of 19 ms
+      double d = c + 40.0/60 * 45 / 100;  // keep going.
 
-      // Full range exp. Didn't want to change a lot of code.
-      double a = 0.0;
-      double b = a + 1.0 *   0.010 / 1000;  // give some time for the latency to stabilize
-      double c = b + 1.0 * 499.995 / 1000;  // begin the SlaAdmin with a target latency of 19 ms
-      double d = c + 1.0 * 499.995 / 1000;  // keep going.
       _simulation_time_1 = _simulation_time_0 + boost::posix_time::time_duration(0, 0, 0, (_simulation_time_4 - _simulation_time_0).total_nanoseconds() / 1000.0 * (a/d));
       _simulation_time_2 = _simulation_time_0 + boost::posix_time::time_duration(0, 0, 0, (_simulation_time_4 - _simulation_time_0).total_nanoseconds() / 1000.0 * (b/d));
       _simulation_time_3 = _simulation_time_0 + boost::posix_time::time_duration(0, 0, 0, (_simulation_time_4 - _simulation_time_0).total_nanoseconds() / 1000.0 * (c/d));
@@ -154,11 +149,15 @@ void Init2() {
       //double c = b + 175.0 * 475 / 1000;
       //double d = c + 175.0 * 475 / 1000;
 
-      double a = 0.0;
-      double b = a + 1.0 *   0.010 / 1000;
-      double c = b + 1.0 * 499.995 / 1000;
-      double d = c + 1.0 * 499.995 / 1000;
+      //double a = 0.0;
+      //double b = a + 1.0 *   0.010 / 1000;
+      //double c = b + 1.0 * 499.995 / 1000;
+      //double d = c + 1.0 * 499.995 / 1000;
 
+      double a = 60.0/100;
+      double b = a + 40.0/100 * 10 / 100;  // give some time for the latency to stabilize
+      double c = b + 40.0/100 * 45 / 100;  // begin the SlaAdmin with a target latency of 19 ms
+      double d = c + 40.0/100 * 45 / 100;  // keep going.
       _simulated_time_1 = _simulated_time_0 + boost::posix_time::time_duration(0, 0, 0, (_simulated_time_4 - _simulated_time_0).total_nanoseconds() / 1000.0 * (a/d));
       _simulated_time_2 = _simulated_time_0 + boost::posix_time::time_duration(0, 0, 0, (_simulated_time_4 - _simulated_time_0).total_nanoseconds() / 1000.0 * (b/d));
       _simulated_time_3 = _simulated_time_0 + boost::posix_time::time_duration(0, 0, 0, (_simulated_time_4 - _simulated_time_0).total_nanoseconds() / 1000.0 * (c/d));
