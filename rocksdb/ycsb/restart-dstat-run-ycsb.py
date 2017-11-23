ab71904177ef6290318547c34e828aeef5606b55
Hobin Yoon <hobinyoon@gmail.com>
2017-11-23 15:05:46 -0500

    sync after aws s3 sync hoping to avoid the corrupted SSTable errors

--- rocksdb/ycsb/restart-dstat-run-ycsb.py
+++ rocksdb/ycsb/restart-dstat-run-ycsb.py
@@ -70,7 +70,7 @@ def YcsbLoad(params, r):
     Util.MkDirs(_dn_log_rocksdb)
 
     if ("use_preloaded_db" in r["load"]) and len(r["load"]["use_preloaded_db"]) > 0:
-      cmd = "aws s3 sync --delete s3://rocksdb-data/%s %s" % (r["load"]["use_preloaded_db"], params["db_path"])
+      cmd = "aws s3 sync --delete s3://rocksdb-data/%s %s ; sync" % (r["load"]["use_preloaded_db"], params["db_path"])
       Util.RunSubp(cmd, measure_time=True, shell=True, gen_exception=False)
       paths = params["db_stg_dev_paths"]
       for i in range(1, len(paths)):
