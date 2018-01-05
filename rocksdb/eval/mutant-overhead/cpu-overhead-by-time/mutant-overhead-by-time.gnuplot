37172e4c98ed7db2e669ddd1e79e6ea207f056f8
Hobin Yoon <hobinyoon@gmail.com>
2018-01-05 00:35:14 -0500

    memory usage. in progress.

--- rocksdb/eval/mutant-overhead/cpu-overhead-by-time/mutant-overhead-by-time.gnuplot
+++ rocksdb/eval/mutant-overhead/cpu-overhead-by-time/mutant-overhead-by-time.gnuplot
@@ -8,10 +8,12 @@ OUT_FN = system("echo $OUT_FN")
 set print "-"
 #print sprintf("TIME_MAX=%s", TIME_MAX)
 
-set terminal pdfcairo enhanced size 3.5in, (2.3*0.85)in
+set terminal pdfcairo enhanced size 3.0in, (2.3*0.65)in
 set output OUT_FN
 
-# Time vs. CPU usage and the total number of SSTables
+LMARGIN = 8.5
+
+# Time vs. total number of SSTables
 if (1) {
   reset
   set xdata time
@@ -19,49 +21,117 @@ if (1) {
   set format x "%1H"
 
   set xlabel "Time (hour)"
-  set ylabel "CPU usage (%)" offset 1,0
-  set y2label "# of SSTables" tc rgb "black"
-  set xtics nomirror tc rgb "black" #autofreq 0,2*3600
+  set ylabel "# of SSTables" offset 0.5, 0
+  set xtics nomirror tc rgb "black"
   set ytics nomirror tc rgb "black"
-  set y2tics nomirror tc rgb "black"
   set grid xtics ytics back lc rgb "#808080"
   set border back lc rgb "#808080" back
+
+  # Align the stacked plots
+  set lmargin LMARGIN
+
+  set xrange ["00:00:00.000":TIME_MAX]
+
+  C_NUM_SSTS = "red"
+  LW_NUM_SSTS = 2
+
+  plot \
+  ROCKSDB u 1:4:3:(0)       w vectors nohead lc rgb C_NUM_SSTS lw LW_NUM_SSTS not, \
+  ROCKSDB u 2:4:(0):($5-$4) w vectors nohead lc rgb C_NUM_SSTS lw LW_NUM_SSTS not
+
+  # vectors: x y xdelta ydelta
+}
+
+
+# Time vs. CPU usage
+if (1) {
+  reset
+  set xdata time
+  set timefmt "%H:%M:%S"
+  set format x "%1H"
+
+  set xlabel "Time (hour)"
+  set ylabel "CPU usage (%)" offset 0.5,0
+  set xtics nomirror tc rgb "black" #autofreq 0,2*3600
+  set ytics nomirror tc rgb "black"
+  set grid xtics ytics back lc rgb "#808080"
+  set border back lc rgb "#808080" back
+
+  set lmargin LMARGIN
 
   set xrange ["00:00:00.000":TIME_MAX]
   set yrange [0:100]
 
-  #C_NUM_SSTS = "#8080FF"
-  C_NUM_SSTS = "#808080"
-  LW_NUM_SSTS = 2
-
-  #C_U = "#808080"
-  #C_U = "black"
   C_U = "blue"
   C_C = "red"
 
   # CPU usage line width
-  C_LW = 3
+  C_LW = 2
 
   # Base indices of columes without and wit CPU
   b0 = 2
   b1 = 10
 
   # x offset in minutes
-  x_offset = 9
+  x_offset = 10
   x0(a) = sprintf("%02d:%02d:00", a, 30 - x_offset)
   x1(a) = sprintf("%02d:%02d:00", a, 30 + x_offset)
 
-  set boxwidth 13 * 60
+  set boxwidth 15 * 60
   PS = 0.4
-  
+
   plot \
-  ROCKSDB u 1:4:3:(0)       w vectors axes x1y2 nohead lc rgb C_NUM_SSTS lw LW_NUM_SSTS not, \
-  ROCKSDB u 2:4:(0):($5-$4) w vectors axes x1y2 nohead lc rgb C_NUM_SSTS lw LW_NUM_SSTS not, \
   CPU_STAT u (x0($1)):(column(b0+3)):(column(b0+2)):(column(b0+6)):(column(b0+5)) w candlesticks whiskerbars lc rgb C_U lw C_LW not, \
   CPU_STAT u (x0($1)):(column(b0)) w p pt 7 ps PS lc rgb C_U not, \
   CPU_STAT u (x1($1)):(column(b1+3)):(column(b1+2)):(column(b1+6)):(column(b1+5)) w candlesticks whiskerbars lc rgb C_C lw C_LW not, \
   CPU_STAT u (x1($1)):(column(b1)) w p pt 7 ps PS lc rgb C_C not
 
-  # vectors: x y xdelta ydelta
+  # candlesticks: x box_min whisker_min whisker_high box_high
+}
+
+
+# TODO: Time vs. memory usage
+if (0) {
+  reset
+  set xdata time
+  set timefmt "%H:%M:%S"
+  set format x "%1H"
+
+  set xlabel "Time (hour)"
+  set ylabel "Memory usage (GB)" offset 0.5,0
+  set xtics nomirror tc rgb "black" #autofreq 0,2*3600
+  set ytics nomirror tc rgb "black"
+  set grid xtics ytics back lc rgb "#808080"
+  set border back lc rgb "#808080" back
+
+  set lmargin LMARGIN
+
+  set xrange ["00:00:00.000":TIME_MAX]
+  set yrange [0:100]
+
+  C_U = "blue"
+  C_C = "red"
+
+  # CPU usage line width
+  C_LW = 2
+
+  # Base indices of columes without and wit CPU
+  b0 = 2
+  b1 = 10
+
+  # x offset in minutes
+  x_offset = 10
+  x0(a) = sprintf("%02d:%02d:00", a, 30 - x_offset)
+  x1(a) = sprintf("%02d:%02d:00", a, 30 + x_offset)
+
+  set boxwidth 15 * 60
+  PS = 0.4
+
+  plot \
+  CPU_STAT u (x0($1)):(column(b0+3)):(column(b0+2)):(column(b0+6)):(column(b0+5)) w candlesticks whiskerbars lc rgb C_U lw C_LW not, \
+  CPU_STAT u (x0($1)):(column(b0)) w p pt 7 ps PS lc rgb C_U not, \
+  CPU_STAT u (x1($1)):(column(b1+3)):(column(b1+2)):(column(b1+6)):(column(b1+5)) w candlesticks whiskerbars lc rgb C_C lw C_LW not, \
+  CPU_STAT u (x1($1)):(column(b1)) w p pt 7 ps PS lc rgb C_C not
+
   # candlesticks: x box_min whisker_min whisker_high box_high
 }
--- rocksdb/eval/mutant-overhead/cpu-overhead-by-time/plot.py
+++ rocksdb/eval/mutant-overhead/cpu-overhead-by-time/plot.py
@@ -25,6 +25,7 @@ def main(argv):
     
 def PlotCpuOverheadByTime():
   fn_cpu_stat_by_time = _GetFnCpuOverhead()
+  fn_mem_stat_by_time = _GetFnMemOverhead()
   fn_rocksdb = _GetFnRocksDB()
   time_max = "09:00:00"
   fn_out = "%s/mutant-overhead.pdf" % Conf.GetOutDir()
@@ -36,46 +37,6 @@ def PlotCpuOverheadByTime():
     env["ROCKSDB"] = fn_rocksdb
     env["OUT_FN"] = fn_out
     Util.RunSubp("gnuplot %s/mutant-overhead-by-time.gnuplot" % os.path.dirname(__file__), env=env)
-    Cons.P("Created %s %d" % (fn_out, os.path.getsize(fn_out)))
-
-
-  sys.exit(0)
-
-  fn_ycsb_log = params[0]
-
-  # 171121-194901/ycsb/171122-010708.903-d
-  mo = re.match(r"(?P<dn_log>.+)/(?P<job_id>\d\d\d\d\d\d-\d\d\d\d\d\d)/ycsb/(?P<exp_dt>\d\d\d\d\d\d-\d\d\d\d\d\d\.\d\d\d).+", fn_ycsb_log)
-  dn_log = mo.group("dn_log")
-  job_id = mo.group("job_id")
-  exp_dt = mo.group("exp_dt")
-  #Cons.P(dn_log)
-  #Cons.P(job_id)
-  #Cons.P(exp_dt)
-
-
-  params_formatted = fn_ycsb_log + "\n" + pprint.pformat(params1[0]) + "\n" + pprint.pformat(params1[1])
-  # The last, space substitution doesn't seem to work all of a sudden. Not the highest priority.
-  params_formatted = params_formatted.replace("\n", "\\n").replace("_", "\\\\_").replace("{", "\{").replace("}", "\}") #.replace(" ", "\\ ")
-  #Cons.P(params_formatted)
-
-  dn_log_job = "%s/%s" % (dn_log, job_id)
-
-  (fn_dstat, num_stgdevs) = DstatLog.GenDataFileForGnuplot(dn_log_job, exp_dt)
-  #Cons.P("%s %s" % (fn_dstat, num_stgdevs))
-  fn_rocksdb = RocksdbLog.GenDataFileForGnuplot(dn_log_job, exp_dt)
-
-  fn_out = "%s/rocksdb-ycsb-all-metrics-by-time-%s.pdf" % (Conf.GetOutDir(), exp_dt)
-
-  with Cons.MT("Plotting ..."):
-    env = os.environ.copy()
-    env["PARAMS"] = params_formatted
-    env["NUM_STG_DEVS"] = str(num_stgdevs)
-    env["TIME_MAX"] = str(time_max)
-    env["IN_FN_DSTAT"] = fn_dstat
-    env["IN_FN_YCSB"] = fn_ycsb
-    env["IN_FN_ROCKSDB"] = fn_rocksdb
-    env["OUT_FN"] = fn_out
-    Util.RunSubp("gnuplot %s/rocksdb-ycsb-all-metrics-by-time.gnuplot" % os.path.dirname(__file__), env=env)
     Cons.P("Created %s %d" % (fn_out, os.path.getsize(fn_out)))
 
 
@@ -159,6 +120,74 @@ def _GetCpuStatByHour(fn_ycsb):
     #Cons.P("%d %s" % (hour, r))
     hour_cpustat[hour] = r
   return hour_cpustat
+
+
+def _GetFnMemOverhead():
+  fn_out = "%s/memory-overhead-by-time" % Conf.GetOutDir()
+  if os.path.exists(fn_out):
+    return fn_out
+
+  dn_base = Conf.GetDir("dn_base")
+  fn_ycsb_0 = "%s/%s" % (dn_base, Conf.Get("unmodified_db"))
+  fn_ycsb_1 = "%s/%s" % (dn_base, Conf.Get("computation_overhead"))
+
+  hour_memstat_0 = _GetMemStatByHour(fn_ycsb_0)
+  hour_memstat_1 = _GetMemStatByHour(fn_ycsb_1)
+  #Cons.P(hour_memstat_0)
+  #Cons.P(hour_memstat_1)
+
+  with open(fn_out, "w") as fo:
+    fo.write("# u: unmodified\n")
+    fo.write("# c: with SSTable access monitoring and SSTable placement computation\n")
+    fo.write("#\n")
+    fmt = "%1d" \
+        " %6.0f %6.0f %6.0f %6.0f %6.0f %6.0f %6.0f %6.0f" \
+        " %6.0f %6.0f %6.0f %6.0f %6.0f %6.0f %6.0f %6.0f"
+    fo.write(Util.BuildHeader(fmt, "hour" \
+        " u_avg u_min u_1 u_25 u_50 u_75 u_99 u_max" \
+        " c_avg c_min c_1 c_25 c_50 c_75 c_99 c_max"
+        ) + "\n")
+    for h, s0 in sorted(hour_memstat_0.iteritems()):
+      s1 = hour_memstat_1[h]
+      fo.write((fmt + "\n") % (h
+        , s0.avg, s0.min, s0._1, s0._25, s0._50, s0._75, s0._99, s0.max
+        , s1.avg, s1.min, s1._1, s1._25, s1._50, s1._75, s1._99, s1.max
+        ))
+  Cons.P("Created %s %d" % (fn_out, os.path.getsize(fn_out)))
+  return fn_out
+
+
+def _GetMemStatByHour(fn_ycsb):
+  fn_dstat = _GenDstat(fn_ycsb)
+
+  col_time = 21
+  col_mem_buff = 13
+  #col_mem_cache = 14
+
+  #Cons.P(fn_dstat)
+  # Bucketize CPU usage
+  #   {hour: [mem_usage]}
+  hour_memusage = {}
+  with open(fn_dstat) as fo:
+    for line in fo:
+      if line.startswith("#"):
+        continue
+      line = line.strip()
+      t = re.split(r" +", line)
+      time0 = t[col_time - 1]
+      mem_buff = int(t[col_mem_buff - 1])
+      #Cons.P("%s %d" % (time0, mem_buff))
+      hour = int(time0.split(":")[0])
+      if hour not in hour_memusage:
+        hour_memusage[hour] = []
+      hour_memusage[hour].append(mem_buff)
+
+  hour_memstat = {}
+  for hour, mem_usage in hour_memusage.iteritems():
+    r = Stat.Gen(mem_usage)
+    #Cons.P("%d %s" % (hour, r))
+    hour_memstat[hour] = r
+  return hour_memstat
 
 
 def _GenDstat(fn_ycsb_log):
